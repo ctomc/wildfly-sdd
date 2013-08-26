a32937f1e8df2c196572232763bd379454f726f5
Tomaz Cerar <tomaz.cerar@gmail.com>
2013-08-26 15:32:17 +0200

    add initial classblacklist detector

---
+++ extension/src/main/java/org/wildfly/extension/sdd/ClassBlackListDetectorProcessor.java
@@ -0,0 +1,103 @@
+package org.wildfly.extension.sdd;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.jboss.as.server.deployment.Attachments;
+import org.jboss.as.server.deployment.DeploymentPhaseContext;
+import org.jboss.as.server.deployment.DeploymentUnit;
+import org.jboss.as.server.deployment.DeploymentUnitProcessingException;
+import org.jboss.as.server.deployment.DeploymentUnitProcessor;
+import org.jboss.as.server.deployment.module.ResourceRoot;
+import org.jboss.modules.filter.PathFilter;
+import org.jboss.modules.filter.PathFilters;
+import org.jboss.vfs.VirtualFile;
+import org.jboss.vfs.VirtualFileFilter;
+import org.jboss.vfs.VisitorAttributes;
+import org.jboss.vfs.util.SuffixMatchFilter;
+
+/**
+ * @author Tomaz Cerar (c) 2013 Red Hat Inc.
+ */
+public class ClassBlackListDetectorProcessor implements DeploymentUnitProcessor {
+
+    private static final String WEB_INF_LIB = "WEB-INF/lib";
+    private static final String EAR_LIB = "lib";
+    private static final VirtualFileFilter JAR_FILTER = new SuffixMatchFilter(".jar", VisitorAttributes.DEFAULT);
+    private final List<String> blackList;
+
+    public ClassBlackListDetectorProcessor(List<String> blacklist) {
+        this.blackList = blacklist;
+        SDDLogger.ROOT_LOGGER.infof("Blacklist: %s", blacklist);
+    }
+
+    @Override
+    public void deploy(DeploymentPhaseContext phaseContext) throws DeploymentUnitProcessingException {
+        final DeploymentUnit deploymentUnit = phaseContext.getDeploymentUnit();
+        final ResourceRoot deploymentRoot = deploymentUnit.getAttachment(Attachments.DEPLOYMENT_ROOT);
+
+
+        List< VirtualFile > jars = getJarList(deploymentRoot.getRoot());
+        try {
+            processBlackList(jars);
+        } catch (IOException e) {
+            SDDLogger.ROOT_LOGGER.couldNotProcessBlacklist(e);
+        }
+
+    }
+
+    @Override
+    public void undeploy(DeploymentUnit context) {
+
+    }
+
+    /*<T> List<T> findIntersection(Set<T> a, Set<T> b) {
+        if (a.size() > b.size()) { return findIntersection(b, a); }
+        List<T> l = new ArrayList<T>();
+        for (T t : a) { if (b.contains(call)) { l.add(call); } }
+        return l;
+    }*/
+
+    private List<VirtualFile> getJarList(final VirtualFile deploymentRoot) throws DeploymentUnitProcessingException {
+        final List<VirtualFile> entries = new ArrayList<>();
+        // WEB-INF lib
+        final VirtualFile webinfLib = deploymentRoot.getChild(WEB_INF_LIB);
+        final VirtualFile earLib = deploymentRoot.getChild(EAR_LIB);
+        try {
+            if (webinfLib.exists()) {
+                final List<VirtualFile> archives = webinfLib.getChildren(JAR_FILTER);
+                for (final VirtualFile archive : archives) {
+                    entries.add(archive);
+
+                }
+            }
+            if (earLib.exists()) {
+                final List<VirtualFile> archives = earLib.getChildren(JAR_FILTER);
+                for (final VirtualFile archive : archives) {
+                    entries.add(archive);
+
+                }
+            }
+        } catch (IOException e) {
+            throw new DeploymentUnitProcessingException("Could get get jar list!", e);
+        }
+        return entries;
+    }
+
+    private void processBlackList(final List<VirtualFile> jars) throws IOException {
+        for (String blackListed : blackList) {
+            PathFilter filter = PathFilters.match(blackListed);
+            for (VirtualFile jar : jars) {
+
+                SDDLogger.ROOT_LOGGER.tracef("Processing jar %s, matches: %s", jar.getName(), filter.accept(jar.getName()));
+
+                if (filter.accept(jar.getName())) {
+                    SDDLogger.ROOT_LOGGER.jarBlacklisted(jar);
+                }
+            }
+
+        }
+
+    }
+}
--- extension/src/main/java/org/wildfly/extension/sdd/SDDSubsystemAdd.java
+++ extension/src/main/java/org/wildfly/extension/sdd/SDDSubsystemAdd.java
@@ -1,11 +1,15 @@
 package org.wildfly.extension.sdd;
 
+import java.util.Arrays;
 import java.util.List;
 
 import org.jboss.as.controller.AbstractBoottimeAddStepHandler;
 import org.jboss.as.controller.OperationContext;
 import org.jboss.as.controller.OperationFailedException;
 import org.jboss.as.controller.ServiceVerificationHandler;
+import org.jboss.as.server.AbstractDeploymentChainStep;
+import org.jboss.as.server.DeploymentProcessorTarget;
+import org.jboss.as.server.deployment.Phase;
 import org.jboss.dmr.ModelNode;
 import org.jboss.msc.service.ServiceController;
 
@@ -23,5 +27,15 @@ public class SDDSubsystemAdd extends AbstractBoottimeAddStepHandler {
     @Override
     protected void performBoottime(OperationContext context, ModelNode operation, ModelNode model, ServiceVerificationHandler verificationHandler, List<ServiceController<?>> newControllers) throws OperationFailedException {
         SDDLogger.ROOT_LOGGER.subsystemStarted();
+
+        context.addStep(new AbstractDeploymentChainStep() {
+                   @Override
+                   protected void execute(DeploymentProcessorTarget processorTarget) {
+
+                       processorTarget.addDeploymentProcessor(SDDExtension.SUBSYSTEM_NAME, Phase.PARSE, Phase.PARSE_WEB_DEPLOYMENT, new ClassBlackListDetectorProcessor(Arrays.asList("javax/servlet","javax/activation")));
+
+
+                   }
+               }, OperationContext.Stage.RUNTIME);
     }
 }
